dcc4ce9f0b9908de1a643bbb1ccd26e345174e6a
Robert Ribeiro <rribeiro@apparatus.net>
2016-01-21 16:46:43 -0500

    setup project

---
+++ JsToolingDemo/Content/demo.controller.js
@@ -0,0 +1,19 @@
+﻿(function() {
+    angular.module('demo', [])
+        .controller('DemoController', DemoController);
+
+    function DemoController($http) {
+        var vm = this;
+
+        vm.go = go;
+
+        function go() {
+            vm.name = null;
+            if (!vm.stop) {
+                $http.get('https://api.github.com/users/azurelogic').then(function(res) {
+                    vm.name = res.data.name;
+                });
+            }
+        }
+    }
+})()
---
+++ JsToolingDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JsToolingDemo.WebApiApplication" Language="C#" %>
